295207f35ff89828103777ff64000d35f6cafb50
Borelio <borelio836@gmail.com>
2022-01-23 01:10:29 +0100

    Started at Welcome-Page

--- FE/src/app/app-routing.module.ts
+++ FE/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomeComponent },
+  // { path: '404', component: E404Component },
+
+  { path: '**', redirectTo: 'welcome' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
