793194c1d5006d1ea22b06e893039f1a73c95017
Borelio <borelio836@gmail.com>
2022-01-29 19:11:21 +0100

    Waiting View

---
+++ FE/src/app/pages/waiting-for-start/waiting-for-start.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-waiting-for-start',
+  templateUrl: './waiting-for-start.component.html',
+  styleUrls: ['./waiting-for-start.component.scss']
+})
+export class WaitingForStartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
