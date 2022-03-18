52dc7747dadd4d9acb8f53cf73e99b2eeffbe53c
Borelio <borelio836@gmail.com>
2022-03-18 22:36:43 +0100

    Removed unnecessary functions in local-storage.service

--- FE/src/app/shared/services/local-storage.service.ts
+++ FE/src/app/shared/services/local-storage.service.ts
@@ -6,22 +6,6 @@ import { LocalStorageConstants } from '../constants/localstorage-constants';
   providedIn: 'root'
 })
 export class LocalStorageService {
-  public get username(): string {
-    return localStorage.getItem(LocalStorageConstants.USERNAME) ?? '';
-  }
-
-  public set username(username: string){
-    localStorage.setItem(LocalStorageConstants.USERNAME, username);
-  }
-
-  public get role(): RoleType {
-    return +(localStorage.getItem(LocalStorageConstants.ROLE) ?? 0);
-  }
-
-  public set role(role: RoleType){
-    localStorage.setItem(LocalStorageConstants.ROLE, (+role).toString());
-  }
-
   public get token(): string {
     return localStorage.getItem(LocalStorageConstants.TOKEN) ?? '';
   }
