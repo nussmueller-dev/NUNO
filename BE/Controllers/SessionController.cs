428d0696e9b3356f51caf8386765c8a3b0ec1f2c
Borelio <borelio836@gmail.com>
2022-03-21 19:13:42 +0100

    Created SessionController

---
+++ BE/Controllers/SessionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NUNO_Backend.Controllers {
+  [Route("sessions")]
+  [ApiController]
+  public class SessionController : ControllerBase {
+    [HttpPost("create")]
+    public IActionResult Create() {
+      
+      
+      return Ok();
+    }
+  }
+}
