20b7ee926ad2a467eb563c90e11eee84b5a71dee
Nico Nussmüller <borelio836@gmail.com>
2022-04-07 19:36:52 +0200

    feat: more cards

--- FE/src/app/pages/play/play.component.ts
+++ FE/src/app/pages/play/play.component.ts
@@ -10,16 +10,32 @@ import { Color } from 'src/app/shared/constants/colors';
   styleUrls: ['./play.component.scss']
 })
 export class PlayComponent implements OnInit {
-  card?: GameCardViewModel;
+  card1?: GameCardViewModel;
+  card2?: GameCardViewModel;
+  card3?: GameCardViewModel;
 
   constructor() { }
 
   ngOnInit(): void {
     let newCrad = new GameCardViewModel();
-    newCrad.cardType = CardType.DrawTwo;
-    newCrad.color = Color.Green;
+    newCrad.cardType = CardType.Reverse;
+    newCrad.color = Color.Yellow;
+    // newCrad.number = 3;
 
-    this.card = newCrad;
+    this.card1 = newCrad;
+
+    let newCrad2 = new GameCardViewModel();
+    newCrad2.cardType = CardType.Number;
+    newCrad2.color = Color.Red;
+    newCrad2.number = 3;
+
+    this.card2 = newCrad2;
+
+    let newCrad3 = new GameCardViewModel();
+    newCrad3.cardType = CardType.Skip;
+    newCrad3.color = Color.Blue;
+
+    this.card3 = newCrad3;
   }
 
 }
