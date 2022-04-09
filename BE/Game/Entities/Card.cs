ce0a64749d676040df4aa880f16c05c57cdb9d81
Nico Nussmüller <borelio836@gmail.com>
2022-04-09 14:18:35 +0200

    fix: fixed card entity

--- BE/Game/Entities/Card.cs
+++ BE/Game/Entities/Card.cs
@@ -2,9 +2,20 @@
 
 namespace Game.Entities {
   public class Card {
-    public int Id { get; set; }
+    public int Id { get; }
     public CardType CardType { get; set; }
-    public ColorType Color { get; set; }
-    public int Number { get; set; }
+    public ColorType? Color { get; set; }
+    public int? Number { get; set; }
+
+    private static int lastCardId = 0;
+
+    public Card(CardType cardType, ColorType? color = null, int? number = null) {
+      Id = lastCardId + 1;
+      lastCardId = Id;
+
+      CardType = cardType;
+      Color = color;
+      Number = number;
+    }
   }
 }
