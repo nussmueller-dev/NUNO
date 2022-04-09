7e45586428db03d94cf903c90fc9765192e86f74
Nico Nussmüller <borelio836@gmail.com>
2022-04-09 14:21:09 +0200

    feat: created GamesController

---
+++ BE/NUNO-Backend/Controllers/GamesController.cs
@@ -0,0 +1,19 @@
+﻿using Authentication.Helpers;
+using Game.Logic;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace NUNO_Backend.Controllers {
+  [Route("games")]
+  [ApiController]
+  public class GamesController : ControllerBase {
+    private readonly SessionLogic _sessionLogic;
+    private readonly CurrentUserHelper _currentUserHelper;
+
+    public GamesController(SessionLogic sessionLogic, CurrentUserHelper currentUserHelper) {
+      _sessionLogic = sessionLogic;
+      _currentUserHelper = currentUserHelper;
+    }
+  }
+}
