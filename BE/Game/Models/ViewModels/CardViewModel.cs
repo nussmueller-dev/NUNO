c055956a9e22f1a407dbd777bf7913e6a64014ed
Nico Nussmüller <borelio836@gmail.com>
2022-04-09 14:20:44 +0200

    fix: fixed CardViewModel

--- BE/Game/Models/ViewModels/CardViewModel.cs
+++ BE/Game/Models/ViewModels/CardViewModel.cs
@@ -5,8 +5,8 @@ namespace Game.Models.ViewModels {
   public class CardViewModel {
     public int Id { get; set; }
     public CardType CardType { get; set; }
-    public ColorType Color { get; set; }
-    public int Number { get; set; }
+    public ColorType? Color { get; set; }
+    public int? Number { get; set; }
 
     public CardViewModel(Card card) { 
       Id = card.Id;
