fb6d50487d48750b2af85166b3ade1c1eba6f611
Nico Nussmüller <borelio836@gmail.com>
2022-04-10 19:32:35 +0200

    feat: created game-card-back.component

---
+++ FE/src/app/shared/components/game-card-back/game-card-back.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-card-back',
+  templateUrl: './game-card-back.component.html',
+  styleUrls: ['./game-card-back.component.scss']
+})
+export class GameCardBackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
