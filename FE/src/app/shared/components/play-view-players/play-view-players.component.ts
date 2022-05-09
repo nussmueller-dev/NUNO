cd17063e3262188cd64a0093575b1a0dce12200e
Nico Nussmüller <borelio836@gmail.com>
2022-05-09 22:12:55 +0200

    feat: kick player mid game

--- FE/src/app/shared/components/play-view-players/play-view-players.component.ts
+++ FE/src/app/shared/components/play-view-players/play-view-players.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
 import { CurrentUserService } from 'src/app/shared/services/current-user.service';
 import { PlayerViewModel } from './../../models/view-models/player-view-model';
+import { PopupService } from './../../services/popup.service';
+import { SessionService } from './../../services/session.service';
 
 @Component({
   selector: 'app-play-view-players',
@@ -11,9 +13,23 @@ export class PlayViewPlayersComponent {
   @Input() players: Array<PlayerViewModel> = [];
   @Input() isReverseDirection: boolean = false;
   @Input() currentPlayerName?: string;
+  @Input() creatorName?: string;
+  @Input() sessionId?: number;
   currentUserService: CurrentUserService;
 
-  constructor(currentUserService: CurrentUserService) {
+  constructor(
+    private popupService: PopupService,
+    private sessionService: SessionService,
+    currentUserService: CurrentUserService
+  ) {
     this.currentUserService = currentUserService;
+  }
+
+  async kick(playerName: string) {
+    if (!await this.popupService.boolQuestionModal.show(`Möchtest du "${playerName}" wirklich aus dem Spiel entfernen?`, 'RAUS!', true)) {
+      return;
+    }
+
+    this.sessionService.kickPlayer(this.sessionId ?? 0, playerName);
   }
 }
